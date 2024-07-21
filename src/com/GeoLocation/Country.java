7297f8d157c1e36503ea4da4107fe9d98220226a
Ayush Mehrotra <ayushmehrotra06092001@gmail.com>
2024-07-21 14:06:49 +0530

    Ayush

---
+++ src/com/GeoLocation/Country.java
@@ -0,0 +1,24 @@
+package com.GeoLocation;
+
+public class Country {
+	
+	String code;
+	double minLat;
+	double maxLat;
+	double minLon;
+	double maxLon;
+	
+	
+	public Country(String code, double minLat, double maxLat, double minLon, double maxLon) {
+		this.code = code;
+		this.minLat = minLat;
+		this.maxLat = maxLat;
+		this.minLon = minLon;
+		this.maxLon = maxLon;
+	}
+	
+	public boolean contains(double lat, double lon) {
+        return lat >= minLat && lat <= maxLat && lon >= minLon && lon <= maxLon;
+    }
+
+}
---
+++ src/com/GeoLocation/Geo_Location_ToCountry.java
@@ -0,0 +1,36 @@
+package com.GeoLocation;
+
+import java.util.*;
+
+public class Geo_Location_ToCountry {
+	
+    private static final List<Country> COUNTRIES = new ArrayList<>();
+    
+    static {
+        COUNTRIES.add(new Country("US", 24.396308, 49.384358, -125.0, -66.93457));
+        COUNTRIES.add(new Country("CA", 41.676555, 83.113883, -141.0, -52.617));
+        COUNTRIES.add(new Country("IN", 6.554607, 35.674545, 68.111379, 97.395358));
+    }
+    
+    public static String getCountryCode(double latitude, double longitude) {
+        for (Country country : COUNTRIES) {
+            if (country.contains(latitude, longitude)) {
+                return country.code;
+            }
+        }
+        return "Unknown";
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter latitude: ");
+        double latitude = scanner.nextDouble();
+        System.out.print("Enter longitude: ");
+        double longitude = scanner.nextDouble();
+
+        String countryCode = getCountryCode(latitude, longitude);
+        System.out.println("Country Code: " + countryCode);
+    }
+
+}
